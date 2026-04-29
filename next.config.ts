d7ef1c68f293621201d5f132b6d87897e89a336e
ManjishPC <manjishupadhaya@gmail.com>
2026-04-29 11:30:21 +0545

    fix again

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   images: {
+    unoptimized: true,
     dangerouslyAllowSVG: true,
     formats: ['image/webp'],
     minimumCacheTTL: 60,
