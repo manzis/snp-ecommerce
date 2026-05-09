0f8b4f702e0c8a84ab1b51f0cdde25e48898fcb5
ManjishPC <manjishupadhaya@gmail.com>
2026-05-10 01:27:48 +0545

    Revert image optimization to fix Vercel limits

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,7 @@ const nextConfig: NextConfig = {
   compress: true, // Enable gzip/brotli compression for all responses
 
   images: {
+    unoptimized: true,
     dangerouslyAllowSVG: true,
     formats: ['image/avif', 'image/webp'],
     minimumCacheTTL: 31536000,
