33c5c9b00c866196e046720e4a0f9fcce699cfeb
ManjishPC <manjishupadhaya@gmail.com>
2026-05-27 23:24:38 +0545

    fix: correct relative path for static hero image import

--- src/components/home/HomeHero.tsx
+++ src/components/home/HomeHero.tsx
@@ -3,7 +3,7 @@
 import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
-import heroBg from '../../../../public/images/heroimage.webp';
+import heroBg from '../../../public/images/heroimage.webp';
 
 interface HomeHeroProps {
     deals?: any[];
